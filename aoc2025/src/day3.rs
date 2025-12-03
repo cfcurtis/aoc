ec567622bbac64fa710868b8c3fd8f72f07b864f
Charlotte Curtis <ccurtis@mtroyal.ca>
2025-12-03 16:04:54 -0700

    Part 1 behaving, part 2 too low

--- aoc2025/src/day3.rs
+++ aoc2025/src/day3.rs
@@ -1,4 +1,3 @@
-use std::error::Error;
 use std::fs;
 
 fn load(filename: &str) -> Vec<Vec<u32>> {
@@ -43,7 +42,43 @@ fn part1(input: &Vec<Vec<u32>>) {
     println!("Total: {}", total);
 }
 
-fn part2(input: &Vec<Vec<u32>>) {}
+fn part2(input: &Vec<Vec<u32>>) {
+    let mut total: u64 = 0;
+    let mut counter = 1;
+
+    for bank in input {
+        let mut bats: Vec<u32> = Vec::with_capacity(12);
+        bats.extend_from_slice(&bank[..12]);
+        let mut cur = 0;
+
+        for (i, it) in bank.iter().enumerate() {
+            let end = i + 12 - cur;
+
+            // copy everything after this value
+            if *it > bats[cur] {
+                bats[cur..].clone_from_slice(&bank[i..end]);
+            }
+
+            // advance cur if there aren't enough numbers left
+            if end > bank.len() - 1 {
+                cur = cur + 1;
+            }
+        }
+
+        // 166003625787367 is too low
+
+        // compose it into a number
+        let mut jolt: u64  = 0;
+        for place in 0..12 {
+            jolt += bats[12 - place - 1] as u64 * 10u64.pow(place as u32);
+        }
+
+        println!("Found {} on line {}", jolt, counter);
+        counter += 1;
+        total += jolt as u64;
+    }
+    println!("Total: {}", total);
+}
 
 pub fn solve(sample: bool) {
     let mut filename = "data/day3/input.txt";
@@ -55,6 +90,6 @@ pub fn solve(sample: bool) {
 
     print!("Part 1: ");
     part1(&input);
-    // print!("Part 2: ");
-    // part2(&input);
+    print!("Part 2: ");
+    part2(&input);
 }
